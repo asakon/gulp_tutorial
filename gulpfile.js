550f8a2148ff45b2813f475d788ecb9219f76253
asakon <kondoasako@gmail.com>
2020-07-07 11:24:03 +0900

    Flexbugs Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+const flexBugsFixes = require('postcss-flexbugs-fixes')
 
 const autoprefixerOption = {
     grid: true
 }
 
-const postcssOption = [autoprefixer(autoprefixerOption)]
+const postcssOption = [
+    flexBugsFixes,
+    autoprefixer(autoprefixerOption)
+]
 
 gulp.task('sass', () => {
    return gulp.src('./src/sass/common.scss')
