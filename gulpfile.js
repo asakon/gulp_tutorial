aa5d7634168d628af1d7aba689a52896e8743646
asakon <kondoasako@gmail.com>
2020-07-07 09:08:11 +0900

    gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')
 
 gulp.task('sass', () => {
-   gulp.src('./src/sass/common.scss')
+   return gulp.src('./src/sass/common.scss')
+   .pipe(sass())
+   .pipe(gulp.dest('./dist'))
 })
