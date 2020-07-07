15b6683f49ff1e162752ddaece048f827e376998
asakon <kondoasako@gmail.com>
2020-07-07 11:16:21 +0900

    PostCSS, Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+
+const autoprefixerOption = {
+    grid: true
+}
+
+const postcssOption = [autoprefixer(autoprefixerOption)]
 
 gulp.task('sass', () => {
    return gulp.src('./src/sass/common.scss')
    .pipe(sass())
+   .pipe(postcss(postcssOption))
    .pipe(gulp.dest('./dist'))
 })
